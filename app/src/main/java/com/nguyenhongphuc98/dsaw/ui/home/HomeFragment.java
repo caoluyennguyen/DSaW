c413f2093ca9731097444530a08ae7288f946e22
nguyenhongphuc98 <nguyenhongphuc98@gmail.com>
2020-04-28 20:49:20 +0700

    add draw chart lib

--- app/src/main/java/com/nguyenhongphuc98/dsaw/ui/home/HomeFragment.java
+++ app/src/main/java/com/nguyenhongphuc98/dsaw/ui/home/HomeFragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.anychart.AnyChartView;
 import com.nguyenhongphuc98.dsaw.R;
 
 public class HomeFragment extends Fragment {
@@ -23,13 +24,8 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        AnyChartView anyChartView = (AnyChartView) root.findViewById(R.id.home_line_chart);
         return root;
     }
 }
