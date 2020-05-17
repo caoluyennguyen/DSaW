b5126341d190da50d6282560aba764c1dfcfa8f4
nguyenhongphuc98 <nguyenhongphuc98@gmail.com>
2020-05-17 15:56:20 +0700

    add answer (forview) model

---
+++ app/src/main/java/com/nguyenhongphuc98/dsaw/data/model/AnswerViewModel.java
@@ -0,0 +1,31 @@
+package com.nguyenhongphuc98.dsaw.data.model;
+
+import java.util.List;
+
+public class AnswerViewModel {
+
+    private String questionTitle;
+
+    private List<String> answers;
+
+    public AnswerViewModel(String questionTitle, List<String> answers) {
+        this.questionTitle = questionTitle;
+        this.answers = answers;
+    }
+
+    public String getQuestionTitle() {
+        return questionTitle;
+    }
+
+    public void setQuestionTitle(String questionTitle) {
+        this.questionTitle = questionTitle;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<String> answers) {
+        this.answers = answers;
+    }
+}
