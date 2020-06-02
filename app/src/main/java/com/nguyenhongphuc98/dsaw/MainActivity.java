ed97e1055bf1ec30c21bb4b5b778268cbe7f7ec7
nguyenhongphuc98 <nguyenhongphuc98@gmail.com>
2020-06-02 10:12:46 +0700

    tracking location base on network -wifi and GPS

--- app/src/main/java/com/nguyenhongphuc98/dsaw/MainActivity.java
+++ app/src/main/java/com/nguyenhongphuc98/dsaw/MainActivity.java
@@ -1,6 +1,13 @@
 package com.nguyenhongphuc98.dsaw;
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.MutableLiveData;
@@ -16,10 +23,24 @@ import com.nguyenhongphuc98.dsaw.data.model.Account;
 import com.nguyenhongphuc98.dsaw.data.network.DataService;
 import com.nguyenhongphuc98.dsaw.utils.Geo;
 import com.nguyenhongphuc98.dsaw.utils.GeoHandle;
+import com.nguyenhongphuc98.dsaw.utils.LocationTrack;
 import com.nguyenhongphuc98.dsaw.utils.Utils;
+
+import java.util.ArrayList;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
     private int mMenuSet = 2;
+
+    // Tracking location
+    private ArrayList<String> permissionsToRequest;
+    private ArrayList permissionsRejected = new ArrayList();
+    private ArrayList permissions = new ArrayList();
+
+    private final static int ALL_PERMISSIONS_RESULT = 101;
+    LocationTrack locationTrack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,17 +90,114 @@ public class MainActivity extends AppCompatActivity {
         GeoHandle handle = new GeoHandle();
         Geo.getAddressFromLocation(10.877898, 106.807128,getApplicationContext(),handle);
 
-//        A a = new A();
-//        a.s ="aaa";
-//        test(a);
-//        Log.d("TAGGG", "onCreate: s="+ a.s);
+        /// Location tracking =======================================================================
+        permissions.add(ACCESS_FINE_LOCATION);
+        permissions.add(ACCESS_COARSE_LOCATION);
+
+        permissionsToRequest = findUnAskedPermissions(permissions);
+        //get the permissions we have asked for before but are not granted..
+        //we will store this in a global list to access later.
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (permissionsToRequest.size() > 0)
+                requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]), ALL_PERMISSIONS_RESULT);
+        }
+
+
+        locationTrack = new LocationTrack(MainActivity.this);
+
+        if (locationTrack.canGetLocation()) {
+
+            double longitude = locationTrack.getLongitude();
+            double latitude = locationTrack.getLatitude();
+
+            Toast.makeText(getApplicationContext(), "Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude), Toast.LENGTH_LONG).show();
+            Log.e("LOCATION", "onCreate: location:"+longitude +"-"+latitude);
+        } else {
+
+            locationTrack.showSettingsAlert();
+        }
+
     }
 
-//    void test(final A a) {
-//        a.s ="bbb";
-//    }
-//
-//    class A {
-//        String s;
-//    }
+    private ArrayList findUnAskedPermissions(ArrayList<String> wanted) {
+        ArrayList result = new ArrayList();
+
+        for (String perm : wanted) {
+            if (!hasPermission(perm)) {
+                result.add(perm);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean hasPermission(String permission) {
+        if (canMakeSmores()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
+            }
+        }
+        return true;
+    }
+
+    private boolean canMakeSmores() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+
+            case ALL_PERMISSIONS_RESULT:
+                for (String perms : permissionsToRequest) {
+                    if (!hasPermission(perms)) {
+                        permissionsRejected.add(perms);
+                    }
+                }
+
+                if (permissionsRejected.size() > 0) {
+
+
+//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                        if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+//                            showMessageOKCancel("These permissions are mandatory for the application. Please allow access.",
+//                                    new DialogInterface.OnClickListener() {
+//                                        @Override
+//                                        public void onClick(DialogInterface dialog, int which) {
+//                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                                                requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), ALL_PERMISSIONS_RESULT);
+//                                            }
+//                                        }
+//                                    });
+//                            return;
+//                        }
+//                    }
+
+                }
+
+                break;
+        }
+
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        locationTrack.stopListener();
+    }
 }
